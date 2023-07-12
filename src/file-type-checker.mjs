d41b884b4a2a20b54666922ed284f47c33718623
devops_automation <136750069+L20234@users.noreply.github.com>
2023-07-12 08:29:09 -0400

    Add all files

---
+++ src/file-type-checker.mjs
@@ -0,0 +1,102 @@
+import { logMinimizer } from "./helpers.mjs"
+import { isText, isBinary } from "istextorbinary"
+import { parseLinkHeader } from '@web3-storage/parse-link-header'
+
+async function fileTypeChecker(core, github, octokit) {
+    // main function to check file types
+    let found_binaries = new Array()
+
+    // this gets a list of file objects containing info about each file that was added or edited (not removed) in the pull request
+    core.info("Getting changed files ...")
+    const changed_files = await getChangedFiles(github.context, octokit, core)
+
+    // this filters out know text files by using file extension. This is a quick check to avoid checking the file type of every file
+    // full list of known text extensions can be found here: https://raw.githubusercontent.com/bevry/textextensions/master/source/index.ts
+    // every file that did not pass the filter will be checked for binary type using files content (blob) as well as its file extension
+    const changed_non_text_files = changed_files.filter(file => !isText(file.filename))
+    core.info(`Found ${changed_non_text_files.length} files that needs explicit file type check as they are not defined in the list of known text files`)
+    logMinimizer(core, "Changed files to explicitly check for a datatype:", changed_non_text_files.map((item) => item.filename))
+
+    // if initial filter marks all files as text, skip the rest of check
+    if (changed_non_text_files.length === 0) { return { result:true, binaries:found_binaries }}
+
+    core.info("Checking file types ...")
+    for (let i = 0; i < changed_non_text_files.length; i++) {
+        const file_path = changed_non_text_files[i].filename
+        const file_sha = changed_non_text_files[i].sha
+        // get file blob and confirm it is a binary file
+        const file_blob = await getFileBlob(github, octokit, file_sha)
+        if (isBinary(file_path, file_blob)) {
+            core.error(`File at path: ${file_path} is a binary file`)
+            found_binaries.push(file_path)
+        }
+    }
+
+    if (found_binaries.length > 0) { return { result:false, binaries:found_binaries }}
+    return { result:true, binaries:found_binaries }
+}
+
+async function getChangedFiles(context, octokit, core) {
+    // Obtain a list of files that were modified in the Pull Request
+    const owner = context.payload.repository.owner.login
+    const repo = context.payload.repository.name
+    const pull_number = context.payload.pull_request.number
+
+    let current_page = 1
+    let full_files_data = []
+
+    // loop over pages of data
+    while (true) {
+        // Get a single page
+        let response = await octokit.rest.pulls.listFiles({
+            owner,
+            repo,
+            pull_number,
+            per_page: 100,
+            page: current_page
+        })
+
+        // add data to a full list
+        full_files_data = full_files_data.concat(response.data)
+
+        // get info about next page
+        // This checks if there is pagination data, as when GitHub only contains data for a less than a single page, there will be no info for next pages at all
+        if(response.headers.link) {
+            const parsed = parseLinkHeader(response.headers.link)
+            if(!parsed.next) {
+                // There are no more pages
+                break
+            }
+        }
+        else {
+            // There is only one page
+            break
+        }
+
+        current_page++
+    }
+
+    // Filter files that were removed from scm and should be ignored
+    const deleted_files_count = full_files_data.filter((item) => item.status === "removed").length
+
+    // Filter files that need to be checked
+    const changed_files = full_files_data.filter((item) => item.status !== "removed")
+
+    core.info(`Total files in a Pull Request: ${full_files_data.length}`)
+    core.info(`Removed files count: ${deleted_files_count}`)
+    core.info(`Added or modified files count: ${changed_files.length}`)
+
+    return changed_files
+}
+
+async function getFileBlob(github, octokit, file_sha) {
+    // get the blob of a file
+    const { data: { content } } = await octokit.rest.git.getBlob({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        file_sha: file_sha
+    })
+    return Buffer.from(content, 'base64')
+}
+
+export { fileTypeChecker }
---
+++ src/main.js
@@ -0,0 +1,149 @@
+import { fileTypeChecker } from "./file-type-checker.mjs"
+import { autoLabeler } from "./auto-labeler.mjs"
+import { logMinimizer, logSeparator } from "./helpers.mjs"
+
+const core = require("@actions/core")
+const github = require("@actions/github")
+const time_start = process.hrtime()
+
+async function main() {
+    // main
+
+    // set execution values
+    const regex_patterns = core.getInput("title_regex").split(";")
+    const gh_token = core.getInput("github_token")
+    const octokit = github.getOctokit(gh_token)
+    const pr_title = github.context.payload.pull_request.title
+    const source_branch = github.context.payload.pull_request.head.ref
+    const source_sha = github.context.payload.pull_request.head.sha
+    const target_branch = github.context.payload.pull_request.base.ref
+    const target_sha = github.context.payload.pull_request.base.sha
+
+    try {
+        // print out relevant available data from received from a github
+        logSeparator(core)
+        logMinimizer(core, "github.context", github.context)
+        logMinimizer(core, "PR Title", pr_title)
+        logMinimizer(core, "HEAD branch", source_branch)
+        logMinimizer(core, "HEAD sha", source_sha)
+        logMinimizer(core, "Base branch", target_branch)
+        logMinimizer(core, "Base sha", target_sha)
+        logSeparator(core)
+
+        // contains encountered errors during execution
+        var exec_errors = new Array()
+        var linter_report = new Array("### :warning: **Pull Request Linter found some issues. Merge is blocked until resolved** :warning:")
+
+        // validate Pull Request title
+        if (!isPrTitleValid(regex_patterns, pr_title)) {
+            let pr_error = "PR Title did not pass regex validation.\nReview title syntax guidelines here:\nhttps://litens.atlassian.net/l/cp/rGxXK6Xx"
+            linter_report.push(pr_error)
+            core.error(pr_error)
+            exec_errors.push(pr_error)
+        }
+        log_timestamp()
+        logSeparator(core)
+
+        // Feature: auto-labeler
+        if (core.getInput("enable_labeler") === "true") {
+            core.info("PR auto-label is enabled for the repo ...")
+            const result = await autoLabeler(core, github, octokit)
+            if (!result) {
+                let auto_labeler_error = "Auto labeler encountered an error"
+                linter_report.push(auto_labeler_error)
+                core.error(auto_labeler_error)
+                exec_errors.push(auto_labeler_error)
+            }
+        }
+        else { core.warning("PR auto-label is disabled for the repo, skipping.") }
+        log_timestamp()
+        logSeparator(core)
+
+        // Feature: file checker
+        if(core.getInput("enable_file_checker") === "true") {
+            core.info("PR file-type-checker is enabled for the repo ...")
+            const { result, binaries } = await fileTypeChecker(core, github, octokit)
+            if (!result) {
+                const binaries_count = binaries.length
+                core.info(`Found ${binaries_count} binary files.`)
+                const binaries_comment = `PR contains ${binaries_count} binary files.\nAddition of binaries to the repo must be approved by repo administrator.\n\`\`\`\n${binaries.join("\n")}\n\`\`\`\n`
+                linter_report.push(binaries_comment)
+                exec_errors.push(`PR contains ${binaries_count} binary files.`)
+            }
+        }
+        else { core.warning("PR file-type-checker is disabled for the repo, skipping.") }
+        log_timestamp()
+        logSeparator(core)
+        // check if execution encountered errors
+        if (exec_errors.length) {
+            // create a comment on a PR if there are errors
+            core.info("Sending a comment to a PR ...")
+            linter_report.push("If you have any questions, please contact DevOps team\nazadeh.ghaffari@litens.com")
+            await createCommentOnPR(github, octokit, linter_report.join("\n- "))
+            log_timestamp()
+            logSeparator(core)
+            throw new Error("Workflow encountered errors, see logs for details!") 
+        }
+        // end of the main block
+        return
+    }
+    catch (error) { core.setFailed(error) }
+}
+
+function isPrTitleValid(regexes, pr_title) {
+    // Validates a PR title against a list of regex patterns
+    // Need to match at least one pattern for it to be valid
+    // Returns true if valid
+    // Returns false if not valid or run into an execution error
+    let pr_title_is_valid = false
+    try {
+        // loop over every defined matching pattern
+        regexes.forEach(pattern => {
+            core.info(`Validating '${pr_title}' against '${pattern}'`)
+
+            // validate against current regex pattern
+            const regex = RegExp(pattern)
+            if (regex.test(pr_title)) {
+                core.info("Match.")
+                pr_title_is_valid = true
+            }
+            else { core.info("Not a match.") }
+        })
+
+        // PR need to match at least one pattern for success
+        if (pr_title_is_valid) {
+            core.info("PR Title matched at least one pattern.")
+            core.info("PR Title is valid.")
+            return true
+        }
+
+        // code hit this block if it did not match against any defined regex patterns
+        core.error("PR Title did not match against any of the allowed regex patterns.")
+        return false
+    }
+    catch (error) {
+        core.error(error)
+        core.error("Something went wrong in regex pattern validation!")
+        core.error("Contact DevOps team <azadeh.ghaffari@litens.com>")
+        return false
+    }
+}
+
+function log_timestamp() {
+    // returns a timestamp in seconds
+    const hrtime_end = process.hrtime(time_start)
+    const seconds = hrtime_end[0] + hrtime_end[1] / 1e9
+    core.info(`Execution time: ${seconds.toFixed(2)} seconds`)
+}
+
+async function createCommentOnPR(github, octokit, comment) {
+    // creates a comment on a PR
+    await octokit.rest.issues.createComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: github.context.payload.pull_request.number,
+        body: comment
+    })
+}
+
+main()
